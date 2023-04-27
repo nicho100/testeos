b283da85e77c9d881f19da374264aef25c9a0390
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-04-27 18:59:52 -0300

    Update user.js

--- controller/user.js
+++ controller/user.js
@@ -1,23 +1,25 @@
-const{getAllUsers,getUserById,addSingleUser}=require("../dao/user")
-
+const getDao = require("../dao")
 const getUsers=async()=>{
-    const users=await getAllUsers()
+    const dao=await getDao()
+    const users=await dao.getAllUsers()
     return users
 }
             
 const addUser=async(user)=>{
+    const dao=await getDao()
     if (user.name){
-        const addedUser=await addSingleUser(user)
+        const addedUser=await dao.addSingleUser(user)
         return addedUser
     }
-        throw new Error("Invalid user")
+        //throw new Error("Invalid user")
 }
     //const delete= async(elementId)=>{
     //    let elementDelete= await this.model.deleteOne({_id:elementId})
     //    console.log(elementDelete)
     //}
 const getById=async(id)=>{
-    const user= await getUserById(id)
+    const dao=await getDao()
+    const user= await dao.getUserById(id)
     return user
 }
 //const update= async(elementId,key,newValue)=>{
