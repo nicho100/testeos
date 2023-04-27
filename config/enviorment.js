846a19d4ae1e8126090889a59e7d52b2280f7e90
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-04-27 19:00:32 -0300

    Update enviorment.js

--- config/enviorment.js
+++ config/enviorment.js
@@ -1,5 +1,4 @@
 require("dotenv").config()
-const { ContenedorArchivo } = require('./controller/contenedorArchivos')
 const args= require("minimist")(process.argv.slice(2))
 //traigo el link de mongo de .env y lo exporto
 const urlMongo=process.env.URL//uso dotenv para guardar el link de la base de datos
