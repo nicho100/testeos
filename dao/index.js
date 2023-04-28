69596e14b8215b212381462809c540eefaa2cc9f
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-04-27 21:32:11 -0300

    Update index.js

--- dao/index.js
+++ dao/index.js
@@ -1,11 +1,31 @@
-const Mongodao= require("./product")
-
+const MongodaoProd= require("./product")
+const MongodaoUser= require("./user")
+const MongodaoChat= require("./chat")
 let mongodao=undefined
 const getDao=async()=>{//conecta a la base de datos
     if(!mongodao){
-        mongodao=new Mongodao()
+        mongodao=new MongodaoProd()
         await mongodao.connect()
     }
     return mongodao
 }
-module.exports=getDao
+
+let mongodao2=undefined
+const getDaos=async()=>{//conecta a la base de datos
+    if(!mongodao2){
+        mongodao2=new MongodaoUser()
+        await mongodao2.connect()
+    }
+    return mongodao2
+}
+
+let mongodao3=undefined
+const getDaoc=async()=>{//conecta a la base de datos
+    if(!mongodao3){
+        mongodao3=new MongodaoChat()
+        await mongodao3.connect()
+    }
+    return mongodao3
+}
+
+module.exports={getDao,getDaos,getDaoc}
